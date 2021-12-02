dcddcbb6e21b9a751f79365c0cf454da635cc1d0
SURFACEPROXJIAN\xingj <xingjianli59@gmail.com>
2021-12-02 18:54:55 +0800

    3Sum

---
+++ Union-find/ThreeSum.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ThreeSum {
+
+    public List<List<Integer>> primSolution(int[] A, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> set = new ArrayList<>();
+        int n = A.length;
+        Arrays.sort(A);
+
+        for (int k = 0; k < n - 2; k++) {
+            int i = k + 1;
+            int j = n - 1;
+            int rest = target - A[k];
+            while (i < j) {
+                if (A[i] + A[j] == rest) {
+                    Collections.addAll(set, A[i], A[j], A[k]);
+                    res.add(set);
+                    i++; 
+                    j--;
+                } else if (A[i] + A[j] < rest) i++;
+                else j++;
+            }
+        }
+        return res;
+    }
+
+    public List<List<Integer>> adSolution(int[] A, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> set = new ArrayList<>();
+        int n = A.length;
+        Arrays.sort(A);
+
+        for (int k = 0; k < n - 2; k++) {
+
+            //-------------------------------
+            if (A[k] > target) break;
+            //------------------------------
+            
+            int i = k + 1;
+            int j = n - 1;
+            int rest = target - A[k];
+            while (i < j) {
+                if (A[i] + A[j] == rest) {
+                    Collections.addAll(set, A[i], A[j], A[k]);
+                    res.add(set);
+
+                    //--------------------------------------------
+                    while (i < j && A[i] == A[i + 1]) i++;
+                    while (i < j && A[j] == A[j - 1]) j--;
+                    //--------------------------------------------
+
+                    i++; 
+                    j--;
+                } else if (A[i] + A[j] < rest) i++;
+                else j++;
+            }
+        }
+        return res;
+    }
+}
