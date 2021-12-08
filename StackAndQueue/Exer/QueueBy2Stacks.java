998dc6077ca92a1178059d045dfb31c844239813
char-1ee <xingjianli59@gmail.com>
2021-12-08 16:24:20 +0800

    Add Queue in two stacks

---
+++ StackAndQueue/Exer/QueueBy2Stacks.java
@@ -0,0 +1,39 @@
+public class QueueBy2Stacks {
+    
+    private StackLLImpl<Integer> inStack;   // Enqueue
+    private StackLLImpl<Integer> outStack;  // Dequeue
+    private int size;
+
+    public QueueBy2Stacks() {
+        inStack = new StackLLImpl();
+        outStack = new StackLLImpl();
+    }
+
+    public void enqueue(Integer item) {
+        inStack.push(item);
+        size++;
+    }
+
+    public int dequeue() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+
+        int item = null; 
+        if (!outStack.isEmpty()) {
+            item = outStack.pop();
+            size--;
+        }
+        return item;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
