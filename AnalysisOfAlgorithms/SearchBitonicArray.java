458c46784862d233926f72214f92c8b12f2a90fc
char-1ee <xingjianli59@gmail.com>
2021-12-17 13:35:28 +0800

    Refactor code

--- AnalysisOfAlgorithms/SearchBitonicArray.java
+++ AnalysisOfAlgorithms/SearchBitonicArray.java
@@ -9,20 +9,19 @@ public class BitonicArraySearch {
             }
         }
 
-        if (A[peak] > tgt)
+        if (A[peak] < tgt)
             return false;
         else if (A[peak] == tgt)
             return true;
         else {
             return binarySearch(A, 0, peak - 1, tgt) || binarySearch(A, peak + 1, n - 1, tgt);
         }
-
         return false;
     }
 
     public boolean binarySearch(int[] arr, int i, int j, int tgt) {
         while (i < j) {
-            int mid = i + (i - j) / 2;
+            int mid = i + (i - j) >> 2;
             if (arr[mid] < tgt)
                 j = mid - 1;
             else if (arr[mid] > tgt)
--- MergeSort/CountInversions.java
+++ MergeSort/CountInversions.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
  * Refer to https://www.baeldung.com/cs/counting-inversions-array
  */
 public class CountInversions {
-    
+
     public int countInvOfFirstType(int[] L, int[] R, int lenL, int lenR) {
-        int cnt = 0;    // count of inversions
-        int j = 0;      // index of R (right)
-        for (int i =0 ; i < lenL; i++) {
+        int cnt = 0;        // count of inversions
+        int j = 0;          // index of R (right)
+        for (int i = 0; i < lenL; i++) {
             while (j < lenR && R[j] < L[i])
                 j++;
             cnt += j;
@@ -18,16 +18,17 @@ public class CountInversions {
 
     public int countInv(int[] A, int lo, int hi) {
         int n = A.length;
-        int cnt = 0;    // total count of inversions
-        if (n <= 1) return 0;
+        int cnt = 0;        // total count of inversions
+        if (n <= 1)
+            return 0;
 
         int lenL = n / 2;
         int lenR = n - lenL;
-        int[] L = Arrays.copyOfRange(A, 0, n/2);
-        int[] R = Arrays.copyOfRange(A, n/2, n);
+        int[] L = Arrays.copyOfRange(A, 0, n / 2);
+        int[] R = Arrays.copyOfRange(A, n / 2, n);
 
         int mid = lo + (hi - lo) / 2;
-        cnt = countInv(L, lo, mid) + countInv(R, mid+1, hi);    // recursive calls
+        cnt = countInv(L, lo, mid) + countInv(R, mid + 1, hi);  // recursive calls
         cnt = cnt + countInvOfFirstType(L, R, lenL, lenR);      // recursive operation
         merge(A, lo, hi);                                       // recursive termination
 
@@ -37,16 +38,20 @@ public class CountInversions {
     private void merge(int[] a, int lo, int hi) {
         int[] aux = new int[a.length];
 
-        for (int k = 0; k <= hi; k++){
+        for (int k = 0; k <= hi; k++) {
             aux[k] = a[k];
         }
-        int mid = lo + (hi - lo) /2;
+        int mid = lo + (hi - lo) / 2;
         int i = lo, j = mid + 1;
         for (int k = lo; k <= hi; k++) {
-            if (aux[j] < aux[i]) a[k] = aux[j++];
-            else if (i > mid) a[k] = aux[j++];
-            else if (j > hi) a[k] = aux[i++];
-            else a[k] = aux[i++];
+            if (aux[j] < aux[i])
+                a[k] = aux[j++];
+            else if (i > mid)
+                a[k] = aux[j++];
+            else if (j > hi)
+                a[k] = aux[i++];
+            else
+                a[k] = aux[i++];
         }
     }
 }
